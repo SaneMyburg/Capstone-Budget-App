c52db5fa446548fd0ef0dfe31a3c3ac15dc71abc
SaneMyburg <sane.myburg@gmail.com>
2023-07-20 09:52:51 +0200

    Create database

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_20_074908) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_expenses", id: false, force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "expense_id", null: false
+    t.index ["category_id", "expense_id"], name: "index_categories_expenses_on_category_id_and_expense_id"
+    t.index ["expense_id", "category_id"], name: "index_categories_expenses_on_expense_id_and_category_id"
+  end
+
+  create_table "expenses", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_expenses_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "expenses", "users"
 end
