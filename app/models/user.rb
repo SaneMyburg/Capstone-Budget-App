8e23d7a885838066ed96b16ba6717ee69224f75b
SaneMyburg <sane.myburg@gmail.com>
2023-09-13 11:59:08 +0200

    Set up render for deployment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :expenses, dependent: :destroy
   has_many :categories, dependent: :destroy
